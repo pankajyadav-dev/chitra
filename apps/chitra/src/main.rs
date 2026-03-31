1272638cd1e507d672f2f77af2528d5bbf096220
Pankaj yadav <yadavpankaj.tech@gmail.com>
2026-03-31 17:19:49 +0000

    Feature
    - Implement the validation in the index command and parse the relative path from the root chitra dir to the path that user want to be indexed

--- apps/chitra/src/main.rs
+++ apps/chitra/src/main.rs
@@ -11,25 +11,25 @@ use tracing::{info, warn};
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
     let args = Cli::parse();
-    info!("chitra, here how can i help you today");
-    info!("{:?}", args);
+    //   info!("chitra, here how can i help you today");
+    // info!("{:?}", args);
     let root_dir_path = env::current_dir()?;
     match args.cmd {
         Command::Init { path } => {
             let dir_path = validate_path(path);
             info!("Dir path {:?}", dir_path);
             let ctx_dir_path = root_dir_path.join(dir_path);
-            info!(
-                "The current dir in which the programm is running {:?}",
-                ctx_dir_path
-            );
+            //      info!(
+            //        "The current dir in which the programm is running {:?}",
+            //      ctx_dir_path
+            // );
             init_chitra(ctx_dir_path).await?;
             info!(".chitra is init completed successfully");
         }
         Command::Index { path } => {
-            info!("Index command trigger {:?}", path);
+            //info!("Index command trigger {:?}", path);
             let dir_path = root_dir_path.join(validate_path(path));
-            info!("The dir passed by the user {:?}", dir_path);
+            //info!("The dir passed by the user {:?}", dir_path);
             let chitra_path = check_chitra_dir(&dir_path).await;
             if let Some(c) = chitra_path {
                 info!("The chitra dir {:?}", c);
@@ -38,11 +38,13 @@ async fn main() -> Result<(), Error> {
                     "The parsered relative path for indexing {:?}",
                     relative_path
                 );
+
+                info!("start the indexing of the {:?}", relative_path);
             } else {
                 warn!("The .chitra not found please create it using ctx init");
                 return Ok(());
             }
-            info!("THe chitra path check completed");
+            //info!("THe chitra path check completed");
         }
     }
     Ok(())
